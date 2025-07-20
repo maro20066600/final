5f5c3519494c3e2bb925748d9452d1e554473b55
marwan mohamed <125691476+maro20066600@users.noreply.github.com>
2025-07-21 02:00:47 +0300

    Update page.tsx

--- ok/src/app/closed/page.tsx
+++ ok/src/app/closed/page.tsx
@@ -23,7 +23,7 @@ export default function ClosedPage() {
       <div className="flex justify-between items-center px-1 py-2 mb-4">
         <div className="w-[90px] md:w-[120px]">
           <Image
-            src="/images/logo R/Blue_M.png"
+            src="/images/logo R/White_M.png"
             alt="M Logo"
             width={120}
             height={120}
@@ -45,7 +45,7 @@ export default function ClosedPage() {
         </div>
         <div className="w-[90px] md:w-[120px]">
           <Image
-            src="/images/LOGO L/Blue_YLY.png"
+            src="/images/LOGO L/White_YLY.png"
             alt="YLY Logo"
             width={120}
             height={120}
